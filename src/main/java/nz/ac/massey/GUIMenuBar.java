c1166f5194688bf74d90f1e0045c7708ed4ee031
twwhite84 <twwhite.pvt@gmail.com>
2023-08-13 19:28:28 +1200

    added search menu

--- src/main/java/nz/ac/massey/GUIMenuBar.java
+++ src/main/java/nz/ac/massey/GUIMenuBar.java
@@ -21,55 +21,90 @@ public class GUIMenuBar {
     // file > new
     JMenuItem menuItemNew = new JMenuItem("New", KeyEvent.VK_N);
     menuFile.add(menuItemNew);
+
     menuItemNew.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemNew, "File > New not implemented yet", "Todo", 0);
       }
+
     });
 
     // file > open
     JMenuItem menuItemOpen = new JMenuItem("Open", KeyEvent.VK_O);
     menuFile.add(menuItemOpen);
     menuItemOpen.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemOpen, "File > Open not implemented yet", "Todo", 0);
       }
+
     });
 
     // file > save
     JMenuItem menuItemSave = new JMenuItem("Save", KeyEvent.VK_S);
     menuFile.add(menuItemSave);
     menuItemSave.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemSave, "File > Save not implemented yet", "Todo", 0);
       }
+
     });
 
     // file > settings
     JMenuItem menuItemSettings = new JMenuItem("Settings", KeyEvent.VK_T);
     menuFile.add(menuItemSettings);
     menuItemSettings.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemSettings, "File > Settings not implemented yet", "Todo", 0);
       }
+
     });
 
     // file > print
     JMenuItem menuItemPrint = new JMenuItem("Print", KeyEvent.VK_P);
     menuFile.add(menuItemPrint);
     menuItemPrint.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemSettings, "File > Print not implemented yet", "Todo", 0);
       }
+
     });
 
     // file > exit
     JMenuItem menuItemExit = new JMenuItem("Exit", KeyEvent.VK_X);
     menuFile.add(menuItemExit);
     menuItemExit.addActionListener(new ActionListener() {
+
+      @Override
       public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(menuItemSettings, "File > Exit not implemented yet", "Todo", 0);
       }
+
+    });
+
+    // search menu
+    JMenu menuSearch = new JMenu("Search");
+    menuSearch.setMnemonic(KeyEvent.VK_S);
+    menuBar.add(menuSearch);
+
+    JMenuItem menuItemSearch = new JMenuItem("Search", KeyEvent.VK_S);
+    menuSearch.add(menuItemSearch);
+    menuItemSearch.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(menuItemSearch, "Search not implemented yet", "Todo", 0);
+      }
+
     });
 
     return menuBar;
