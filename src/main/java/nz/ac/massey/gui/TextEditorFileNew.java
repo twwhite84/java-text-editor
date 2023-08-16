4586908b8d1a96d3242eb549badde74e84493e6b
twwhite84 <twwhite.pvt@gmail.com>
2023-08-16 18:36:38 +1200

    added class comment for texteditorfilenew

--- src/main/java/nz/ac/massey/gui/TextEditorFileNew.java
+++ src/main/java/nz/ac/massey/gui/TextEditorFileNew.java
@@ -2,6 +2,9 @@ package nz.ac.massey.gui;
 
 import nz.ac.massey.TextEditor;
 
+/**
+ * Class for opening a new window
+ */
 public class TextEditorFileNew {
   public void menuFileNew() {
     TextEditor.main(null);
