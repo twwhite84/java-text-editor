10d8ff95c7cef9bfac7677023cbe21c47fbddee0
twwhite84 <twwhite.pvt@gmail.com>
2023-08-19 14:13:03 +1200

    switched from dialog to internal frame

--- src/main/java/nz/ac/massey/action/SearchAction.java
+++ src/main/java/nz/ac/massey/action/SearchAction.java
@@ -1,6 +1,14 @@
 package nz.ac.massey.action;
 
-import javax.swing.JOptionPane;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import nz.ac.massey.gui.TextEditorGUI;
 
@@ -12,9 +20,56 @@ public class SearchAction extends TextEditorAction {
 
   @Override
   public void performAction(TextEditorGUI gui) {
-    String query = JOptionPane.showInputDialog(null, "Search for...", "Search", JOptionPane.PLAIN_MESSAGE, null, null,
-        null).toString();
-    System.out.println(query.toString());
+    /*
+     * String query = JOptionPane.showInputDialog(null, "Search for...", "Search",
+     * JOptionPane.PLAIN_MESSAGE, null, null,
+     * null).toString();
+     * 
+     * // just find the first instance matching search. highlight it in editor.
+     * // gui.getGuiContentPane().getTextArea().
+     * String myText = gui.getGuiContentPane().getTextArea().getText();
+     * System.out.println(myText.indexOf(query));
+     * 
+     * //need to highlight
+     */
+
+    JInternalFrame searchFrame = new JInternalFrame("Search", false, true);
+
+    JPanel searchPanel = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+
+    c.gridx = 0;
+    c.gridy = 0;
+    JLabel lblSearch = new JLabel("Search");
+    searchPanel.add(lblSearch, c);
+
+    c = new GridBagConstraints();
+    c.gridx = 0;
+    c.gridy = 1;
+    JTextField txtSearchField = new JTextField(20);
+    searchPanel.add(txtSearchField, c);
+
+    c = new GridBagConstraints();
+    c.gridx = 1;
+    c.gridy = 0;
+    JButton btnSearchNext = new JButton("Next");
+    searchPanel.add(btnSearchNext);
+
+    c = new GridBagConstraints();
+    c.gridx = 1;
+    c.gridy = 1;
+    JButton btnSearchPrev = new JButton("Prev");
+    searchPanel.add(btnSearchPrev);
+
+    // components here
+
+    searchFrame.setContentPane(searchPanel);
+    searchFrame.pack();
+
+    c = new GridBagConstraints();
+    c.anchor = GridBagConstraints.NORTH;
+    gui.getGuiContentPane().add(searchFrame, c);
+    searchFrame.setVisible(true);
   }
 
 }
