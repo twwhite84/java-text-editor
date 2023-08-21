af6634e441996a69de6f1a20eecaf6819b359394
twwhite84 <twwhite.pvt@gmail.com>
2023-08-21 18:36:49 +1200

    using save state from texteditorgui

--- src/main/java/nz/ac/massey/action/ExitAction.java
+++ src/main/java/nz/ac/massey/action/ExitAction.java
@@ -12,25 +12,28 @@ public class ExitAction extends TextEditorAction {
 
     @Override
     public void performAction(TextEditorGUI gui) {
-        if (gui.getOpenFile() == null && gui.getGuiContentPane().getTextArea().getText().length() == 0) {
-            gui.getFrame().dispose();
-        } else {
-            int choice = JOptionPane.showOptionDialog(gui.getFrame(), "Save before exiting?", "Exit",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-            switch (choice) {
-                case JOptionPane.YES_OPTION:
-                    gui.save();
-                    gui.getFrame().dispose();
-                    break;
+        if (!gui.isSaved()) {
+            if (gui.getOpenFile() == null && gui.getGuiContentPane().getTextArea().getText().length() == 0) {
+                gui.getFrame().dispose();
+            } else {
+                int choice = JOptionPane.showOptionDialog(gui.getFrame(), "Save before exiting?", "Exit",
+                        JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+                switch (choice) {
+                    case JOptionPane.YES_OPTION:
+                        gui.save();
+                        gui.getFrame().dispose();
+                        break;
 
-                case JOptionPane.NO_OPTION:
-                    gui.getFrame().dispose();
-                    break;
+                    case JOptionPane.NO_OPTION:
+                        gui.getFrame().dispose();
+                        break;
 
-                default:
-                    break;
+                    default:
+                        break;
+                }
             }
+        } else {
+            gui.getFrame().dispose();
         }
     }
-
 }
