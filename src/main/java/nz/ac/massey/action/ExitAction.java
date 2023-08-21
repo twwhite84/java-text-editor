84f9c54cdf7379e3e04e772ac85209f32ae247e1
twwhite84 <twwhite.pvt@gmail.com>
2023-08-21 18:18:55 +1200

    modal options implemented

--- src/main/java/nz/ac/massey/action/ExitAction.java
+++ src/main/java/nz/ac/massey/action/ExitAction.java
@@ -1,8 +1,6 @@
 package nz.ac.massey.action;
 
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
 
 import nz.ac.massey.gui.TextEditorGUI;
 
@@ -17,21 +15,19 @@ public class ExitAction extends TextEditorAction {
         if (gui.getOpenFile() == null && gui.getGuiContentPane().getTextArea().getText().length() == 0) {
             gui.getFrame().dispose();
         } else {
-            // do you wish to save before exiting?
-            // save, dont save, cancel
             int choice = JOptionPane.showOptionDialog(gui.getFrame(), "Save before exiting?", "Exit",
                     JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
             switch (choice) {
                 case JOptionPane.YES_OPTION:
-                    System.out.println("user chose yes");
+                    gui.save();
+                    gui.getFrame().dispose();
                     break;
 
                 case JOptionPane.NO_OPTION:
-                    System.out.println("user chose no");
+                    gui.getFrame().dispose();
                     break;
 
                 default:
-                    System.out.println("cancel");
                     break;
             }
         }
