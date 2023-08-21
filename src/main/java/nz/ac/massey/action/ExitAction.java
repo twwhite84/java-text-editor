a4b2a5fd1e4ba7f5962be963b313624f2778ef18
twwhite84 <twwhite.pvt@gmail.com>
2023-08-21 18:11:13 +1200

    added exit modal

--- src/main/java/nz/ac/massey/action/ExitAction.java
+++ src/main/java/nz/ac/massey/action/ExitAction.java
@@ -1,23 +1,40 @@
 package nz.ac.massey.action;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
 import nz.ac.massey.gui.TextEditorGUI;
 
 public class ExitAction extends TextEditorAction {
 
-  public ExitAction() {
-    super("Exit");
-  }
-
-  @Override
-  public void performAction(TextEditorGUI gui) {
-    if (gui.getOpenFile() == null && gui.getGuiContentPane().getTextArea().getText().length() > 0) {
-      // do you wish to save before exiting?
-      // save, dont save, cancel
-    } else {
-      System.out.println("should ask to save again and quit");
+    public ExitAction() {
+        super("Exit");
     }
-  }
+
+    @Override
+    public void performAction(TextEditorGUI gui) {
+        if (gui.getOpenFile() == null && gui.getGuiContentPane().getTextArea().getText().length() == 0) {
+            gui.getFrame().dispose();
+        } else {
+            // do you wish to save before exiting?
+            // save, dont save, cancel
+            int choice = JOptionPane.showOptionDialog(gui.getFrame(), "Save before exiting?", "Exit",
+                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+            switch (choice) {
+                case JOptionPane.YES_OPTION:
+                    System.out.println("user chose yes");
+                    break;
+
+                case JOptionPane.NO_OPTION:
+                    System.out.println("user chose no");
+                    break;
+
+                default:
+                    System.out.println("cancel");
+                    break;
+            }
+        }
+    }
 
 }
