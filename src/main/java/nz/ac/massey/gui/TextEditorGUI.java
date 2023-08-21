7826f17122241c3a753d64da886eaffad66bab34
twwhite84 <twwhite.pvt@gmail.com>
2023-08-21 19:01:14 +1200

    close button on window works

--- src/main/java/nz/ac/massey/gui/TextEditorGUI.java
+++ src/main/java/nz/ac/massey/gui/TextEditorGUI.java
@@ -7,6 +7,9 @@ import org.odftoolkit.odfdom.doc.OdfDocument;
 import org.odftoolkit.odfdom.doc.OdfTextDocument;
 
 import javax.swing.*;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -143,13 +146,14 @@ public class TextEditorGUI {
 
             if (System.getenv("GITHUB_ACTIONS") == null) {
                 // Error while saving
-                JOptionPane.showMessageDialog(frame, "There was an error trying to save", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(frame, "There was an error trying to save", "Error",
+                        JOptionPane.ERROR_MESSAGE);
             } else {
                 System.err.println("There was an error trying to save");
             }
         }
     }
-    
+
     /**
      * Open a file and populate the contents of the editor
      *
@@ -186,7 +190,8 @@ public class TextEditorGUI {
             }
         } catch (Exception ex) {
             if (System.getenv("GITHUB_ACTIONS") == null) {
-                JOptionPane.showMessageDialog(frame, "There was an error attempting to open that file", "Error", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(frame, "There was an error attempting to open that file", "Error",
+                        JOptionPane.ERROR_MESSAGE);
             } else {
                 System.err.println("There was an error attempting to open that file");
             }
@@ -209,18 +214,32 @@ public class TextEditorGUI {
         if (System.getenv("GITHUB_ACTIONS") == null) {
             if (saved) {
                 // Update title to saved state
-                if (this.openFile != null) frame.setTitle(this.openFile.getName());
+                if (this.openFile != null)
+                    frame.setTitle(this.openFile.getName());
             } else {
-                if (this.openFile != null) frame.setTitle(this.openFile.getName() + "*");
+                if (this.openFile != null)
+                    frame.setTitle(this.openFile.getName() + "*");
             }
         }
+    }
+
+    /**
+     * exit current window
+     */
+    public void exit() {
+        new ExitAction().performAction(this);
     }
 
     /**
      * Setup the main GUI for a new instance
      */
     private void init() {
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                exit();
+            }
+        });
         frame.setJMenuBar(guiMenuBar);
         frame.setContentPane(guiContentPane);
         frame.setSize(900, 400);
