a1df123419c45a1f62a937e6ed0285be32510033
twwhite84 <twwhite.pvt@gmail.com>
2023-08-23 17:35:59 +1200

    pdf conversion and print dialog

--- src/main/java/nz/ac/massey/action/PrintAction.java
+++ src/main/java/nz/ac/massey/action/PrintAction.java
@@ -1,9 +1,13 @@
 package nz.ac.massey.action;
 
-import javax.swing.JTextArea;
-
+import java.awt.print.PrinterJob;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
 import nz.ac.massey.gui.TextEditorGUI;
 
+/**
+ * Converts editor content to PDF and sends to selected print service
+ */
 public class PrintAction extends TextEditorAction {
 
     public PrintAction() {
@@ -12,9 +16,13 @@ public class PrintAction extends TextEditorAction {
 
     @Override
     public boolean performAction(TextEditorGUI gui) {
-        JTextArea textArea = gui.getGuiContentPane().getTextArea();
         try {
-            textArea.print();
+            PDDocument pddoc = gui.getPdfFile();
+            PrinterJob job = PrinterJob.getPrinterJob();
+            job.setPageable(new PDFPageable(pddoc));
+            if (job.printDialog()) {
+                job.print();
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
